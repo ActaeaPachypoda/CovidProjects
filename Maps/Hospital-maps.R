96c31614b3a7569f5682aff6e157d3d09a2d5f5e
Sarah Fondots <actaea.p@gmail.com>
2020-07-12 19:38:37 -0400

    added county data

--- Maps/Hospital-maps.R
+++ Maps/Hospital-maps.R
@@ -10,19 +10,23 @@ options(scipen = 999)
 
 data <- readr::read_csv("Data/Hospitals.csv")
 
+countyMap_sf <- st_read("Data/cb_2018_us_county_500k/cb_2018_us_county_500k.shp", stringsAsFactors = FALSE)
+countyMap_sf <- countyMap_sf %>%
+  mutate(GEOID = as.numeric(GEOID))
+
 mymap <- st_read("Data/cb_2018_us_zcta510_500k/cb_2018_us_zcta510_500k.shp", stringsAsFactors = FALSE)
 mymap <- mymap %>%
   mutate(ZCTA5CE10 = as.numeric(ZCTA5CE10))
 
-map_and_data <- inner_join(data, mymap)
+country_map_and_data <- inner_join(countyMap_sf, data)
 
-testing <- ggplot(map_and_data) +
-  geom_sf(aes(fill = BEDS))
-testing
+map_and_data <- inner_join(mymap, data)
 
-ggplot(map_and_data) +
-  geom_sf(aes(fill = BEDS)) +
-  scale_fill_gradient (low = "#56B1F7", high = "#132B43")
+tmap_mode("view")
 
-tm_shape(map_and_data) +
+beds <- tm_shape(map_and_data) +
   tm_polygons("BEDS", id = "ZCTA5CE10", palette = "Greens")
+
+countyBeds <- tm_shape(country_map_and_data)+
+  tm_polygons("BEDS", id = "GEOID", palette = "Greens")
+countyBeds
